c5feca487a381f0e194156e81c6b6a950657d32d
Kseniya Mikhailiuk <Kseniya.Mikhailiuk@itechart-group.com>
2019-01-08 17:15:49 +0300

    add user database

---
+++ front/src/services/api/userInfoFetch.js
@@ -0,0 +1,68 @@
+import v4 from 'uuid'
+
+var userInfo = [
+    {
+        id: v4(),
+        name: "Kate",
+        surname: "lal",
+        email: "kate@gmail.com",
+        password: "123456"
+    },
+    {
+        id: v4(),
+        name: "Anna",
+        surname: "qwerty",
+        email: "anna@gmail.com",
+        password: "qwerty"
+    },
+    {
+        id: v4(),
+        name: "Tom",
+        surname: "zxcv",
+        email: "tom@gmail.com",
+        password: "poiuy"
+    },
+    {
+        id: v4(),
+        name: "Bob",
+        surname: "pampam",
+        email: "bob@gmail.com",
+        password: "mnbvc"
+    }
+]
+
+export default userInfo;
+
+const delay = (ms) =>
+    new Promise(resolve => setTimeout(resolve, ms));
+
+export const registerUser = (userData) =>
+    delay(500)
+    .then(() => {
+        let existedUser = userInfo.find(user => user.email === userData.email);
+        if (!existedUser) {
+            userInfo.push({
+                id: v4(),
+                ...userData
+            });
+            return true;
+        }
+        return false;
+    })
+
+export const authorizeUser = (userData) =>
+    delay(500)
+    .then(() => {
+        let response = {
+            isExistedUser: false,
+            isCorrectPassword: false
+        }
+        let existedUser = userInfo.find(user => user.email === userData.email);
+        if (existedUser) {
+            response.isExistedUser = true;
+            if (existedUser.password === userData.password) {
+                response.isCorrectPassword = true;
+            }
+        }
+        return response;
+    })
