e73991887ceace372c64c232c10735b1cc942d0f
Kseniya Mikhailiuk <Kseniya.Mikhailiuk@itechart-group.com>
2019-01-16 17:16:36 +0300

    remove api project

--- back/CinemaReservation.PresentationLayer/Models/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CinemaReservation.PresentationLayer.Models
-{
-    public class LoginRequest
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
